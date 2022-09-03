b53ef446f62b24669d29819fc934f68e405850e0
Jawwad926 <mdsabbirhosen926@gmail.com>
2022-09-03 10:55:27 +0600

    delete comment

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import BackwardCounter from "./components/BackwardCounter";
 import ForwardCounter from "./components/ForwardCounter";
 
-// Changes a little bit to app.js for testing
-
 const App = () => {
   return (
     <React.Fragment>
